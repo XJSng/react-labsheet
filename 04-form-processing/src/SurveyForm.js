d86706a5d3d319c5ba053ba708408712b8723618
Xu Jie Sng <sngxujie@gmail.com>
2024-03-09 04:42:18 +0000

    checkboxes in react

--- 04-form-processing/src/SurveyForm.js
+++ 04-form-processing/src/SurveyForm.js
@@ -4,6 +4,49 @@ export default function SurveyForm() {
     const [name, setName] = useState("")
     const [color, setColor] = useState("")
     const [country, setCountry] = useState("singapore")
+    const [fruits, setFruit] = useState([]) //empty array
+
+    const handleCheckBoxChange = (event) => {
+        if (event.target.checked) {
+            // how to add a value to a state variable
+            // This is called functional programming
+            // // 1. clone the array
+            //     const cloned = fruits.slice()
+            // // 2. update the clone
+            //     cloned.push(event.target.value)
+            // // 3. replace the clone into the state
+            //     setFruit(cloned)
+
+            // Method 2 Preferred method
+            // We spread the array with [...fruit]
+            const cloned = [...fruits, event.target.value]
+            setFruit(cloned)
+
+            // There is a problem.
+
+        } else {
+            // // 1. clone the array
+            // const cloned = fruits.slice()
+
+            // // 2. find the index of the element we are removing
+            // const indexToRemove = fruits.findIndex(f => f == event.target.value);
+
+            // // 3. delete the index from the cloned array
+            // cloned.splice(indexToRemove, 1);
+
+            // // 4. replace the modified array into the state
+            // setFruit(cloned);
+
+            // Method 2
+            const cloned = [...fruits.slice(0, indexToRemove), // This removes everything left of the fruit we want to remove
+                            ...fruits.slice(indexToRemove + 1)]// This removes everything right of the fruit we want to remove
+            // Imagine we have this array of fruits.
+            setFruits(cloned);
+        }
+    }
+
+
+
     return <>
         <h1>Survey Form</h1>
         <div>
@@ -14,8 +57,8 @@ export default function SurveyForm() {
                     // all `onChange` event handlers take one parameter
                     // - the event. The event contains every info about the change
                     // like which key is pressed, which form control etc. etc.
-                    setName(()=> e.target.value)
-                }}/>
+                    setName(() => e.target.value)
+                }} />
         </div>
         <div>
             <label>Favourite Colour:</label>
@@ -35,7 +78,7 @@ export default function SurveyForm() {
                 checked={color == "yellow"}
                 onChange={() => {
                     setColor("yellow")
-                }}/><label>Yellow</label>
+                }} /><label>Yellow</label>
             <input type='radio'
                 value="green"
                 className='ms-2 me-2'
@@ -48,13 +91,50 @@ export default function SurveyForm() {
         </div>
         <div>
             <label>Country</label>
-            <select value={country} className='form-control' onChange={(event)=>{
+            <select value={country} className='form-control' onChange={(event) => {
                 setCountry(event.target.value)
             }}>
                 <option value="singapore">Singapore</option>
                 <option value="malaysia">Malaysia</option>
                 <option value="indonesia">Indonesia</option>
             </select>
+        </div>
+        <div>
+            <label>Fruit</label>
+            <input type='checkbox'
+                name="fruits"
+                value="apple"
+                className='ms-2 me-2'
+                checked={fruits.includes('apple')}
+                onChange={handleCheckBoxChange}
+            /><label>Apple</label>
+
+            <input type="checkbox"
+                name="fruits"
+                value="orange"
+                className='ms-2 me-2'
+                checked={fruits.includes('orange')}
+                onChange={handleCheckBoxChange}
+            />
+            <label>Orange</label>
+
+            <input type="checkbox"
+                name="fruits"
+                value="pineapple"
+                className='ms-2 me-2'
+                checked={fruits.includes('pineapple')}
+                onChange={handleCheckBoxChange}
+            />
+            <label>Pineapple</label>
+
+            <input type="checkbox"
+                name="fruits"
+                value="durian"
+                className='ms-2 me-2'
+                checked={fruits.includes('durian')}
+                onChange={handleCheckBoxChange}
+            />
+            <label>Durian</label>
         </div>
     </>
 }
